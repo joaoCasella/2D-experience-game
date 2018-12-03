3ef91c96aba6e2bbdb754978a840dc7db7ab273c
João Pedro <jpedrocaselladepaula@hotmail.com>
2018-12-02 22:21:26 -0200

    Begun the scripts to control the floor movement and added some tags

---
+++ 2d-experience/Assets/Scripts/FloorController.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FloorController : MonoBehaviour {
+    private float speed = .1f;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(speed * Vector2.left);
+    }
+}
---
+++ 2d-experience/Assets/Scripts/FloorLimitDetection.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FloorLimitDetection : MonoBehaviour {
+
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        Debug.Log(collision.gameObject.tag);
+        if (collision.gameObject.tag == "Floor")
+        {
+            Destroy(collision.gameObject);
+        }
+    }
+}
